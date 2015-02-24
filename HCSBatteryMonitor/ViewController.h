0409ed12419134502a0c21f7178d2fbfc2486fda
Sahil Kapoor <sahilkapoor1109@gmail.com>
2015-02-24 22:56:19 +0530

    Created Project

---
+++ HCSBatteryMonitor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCSBatteryMonitor
+//
+//  Created by Sahil Kapoor on 24/02/15.
+//  Copyright (c) 2015 Hot Cocoa Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HCSBatteryMonitor/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  HCSBatteryMonitor
+//
+//  Created by Sahil Kapoor on 24/02/15.
+//  Copyright (c) 2015 Hot Cocoa Software. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
